8e8c7446bc639e2684e0e78cf3d40c8ed5635fbe
Jonathan Meij <jonathanvanmeij@gmail.com>
2024-09-16 11:29:27 +0200

    added homepage welcome

--- frontend/src/pages/events/eventsPage.tsx
+++ frontend/src/pages/events/eventsPage.tsx
@@ -1,10 +1,18 @@
+import { AuthContext } from "@/contexts/authContext";
 import { useGetEvents } from "@/queries/events";
+import { useContext } from "react";
 
 export default function EventsPage() {
   const { data, isLoading, isError } = useGetEvents();
+  const { userData, isAuthenticated } = useContext(AuthContext);
 
   return (
     <div>
+      {isAuthenticated && (
+        <div>
+          <div>Welcome {userData?.firstName}</div>
+        </div>
+      )}
       {isLoading && <div>Loading...</div>}
       {isError && <div>Error</div>}
       {data && data.map((event) => <div key={event.id}>{event.title}</div>)}
